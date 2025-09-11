820576eb0932d9d835f0283ad13db414c4356d1f
-g <-g>
2025-09-11 16:25:30 +0500

    salom

--- components/providers/db.ts
+++ components/providers/db.ts
@@ -4,4 +4,4 @@ declare global {
     var prisma : PrismaClient | undefined
 }
 export const db = global.prisma || new PrismaClient();
-if(process.env.NODE_ENV === 'development') global.prisma = db
+if(process.env.NODE_ENV === 'development') global.prisma = db 
